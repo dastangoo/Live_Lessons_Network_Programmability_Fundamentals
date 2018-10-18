ff00450c35ade6492349a759c8bd37b325f47af8
dastangoo <hamed.dastangoo@gmail.com>
2018-10-18 20:56:45 +0330

    Add some examples

---
+++ Example01.py
@@ -0,0 +1,5 @@
+x = int(input("Enter the number to which you wish to count: "))
+y = 1
+while y <= x:
+  print(y)
+  y += 1
---
+++ Example02.py
@@ -0,0 +1,7 @@
+x = int(input("Enter the number to which you wish to count: "))
+y = 1
+while True:
+  print(y)
+  y += 1
+  if y > x:
+    break
